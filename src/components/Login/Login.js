94aaadfe614e10f20bd7e962a2785495745334d9
JCameren <johnsonm.cameren@gmail.com>
2022-08-10 17:15:11 -0400

    gave reducers there own file to separate concerns and clean up Login file

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,46 +1,46 @@
 import React, { useEffect, useState, useReducer, useContext } from 'react';
-
+import { emailReducer, passwordReducer } from '../reducers/FormReducers';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import { AuthContext } from '../../context/auth-context';
 
-const emailReducer = (state, action) => {
-  if(action.type === 'USER_INPUT') {
-    return { 
-      value: action.val, 
-      isValid: action.val.includes('@') }
-  }
-  if(action.type === 'INPUT_BLUR') {
-    return {
-      value: state.value, 
-      isValid: state.value.includes('@')
-    }
-  }
-  return { 
-    value: '', 
-    isValid: false }
-}
+// const emailReducer = (state, action) => {
+//   if(action.type === 'USER_INPUT') {
+//     return { 
+//       value: action.val, 
+//       isValid: action.val.includes('@') }
+//   }
+//   if(action.type === 'INPUT_BLUR') {
+//     return {
+//       value: state.value, 
+//       isValid: state.value.includes('@')
+//     }
+//   }
+//   return { 
+//     value: '', 
+//     isValid: false }
+// }
 
-const passwordReducer = (state, action) => {
-  if(action.type === 'USER_INPUT') {
-    return {
-      value: action.val,
-      isValid: action.val.trim().length > 6
-    }
-  }
+// const passwordReducer = (state, action) => {
+//   if(action.type === 'USER_INPUT') {
+//     return {
+//       value: action.val,
+//       isValid: action.val.trim().length > 6
+//     }
+//   }
 
-  if (action.type === 'INPUT_BLUR') {
-    return {
-      value: state.value,
-      isValid: state.value.trim().length > 6
-    }
-  }
-  return {
-    value: '',
-    isValid: false
-  }
-}
+//   if (action.type === 'INPUT_BLUR') {
+//     return {
+//       value: state.value,
+//       isValid: state.value.trim().length > 6
+//     }
+//   }
+//   return {
+//     value: '',
+//     isValid: false
+//   }
+// }
 
 const Login = () => {
   const authCtx = useContext(AuthContext)
